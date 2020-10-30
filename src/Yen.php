782ec1e1c555bc2b095164318c066457ac467157
motoki-ito <motoki.ito@excite.jp>
2020-10-30 17:05:40 +0900

    [mod] Yenクラス追加

---
+++ src/Yen.php
@@ -0,0 +1,18 @@
+<?php
+declare(strict_types=1);
+
+namespace App;
+
+
+class Yen
+{
+    /**
+     * Yenクラスのコンストラクタ
+     *
+     * @param int $amount
+     */
+    public function __construct(private int $amount)
+    {
+
+    }
+}
