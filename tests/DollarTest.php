5d6e58f2e1758bd16608d0f9d0bf6d9fe0d85aa9
takumi-ito-excite <takumi.ito@excite.jp>
2020-11-06 17:18:07 +0900

    [clean] ExChangeTestをYenとDollarのテストへ移動

--- tests/DollarTest.php
+++ tests/DollarTest.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 namespace AppTest;
 
 use App\Dollar;
+use App\Yen;
 use PHPUnit\Framework\TestCase;
 
 class DollarTest extends TestCase
@@ -12,5 +13,16 @@ class DollarTest extends TestCase
     {
         $this->assertSame(100, (new Dollar(100))->toInt());
         $this->assertSame(200, (new Dollar(200))->toInt());
+    }
+
+    public function test_ドルを円に変換するテスト()
+    {
+        $dollar1 = new Dollar(1);
+        $yen100 = new Yen(100);
+        $this->assertSame($yen100->toInt(), $dollar1->toYen()->toInt());
+
+        $dollar2 = new Dollar(2);
+        $yen200 = new Yen(200);
+        $this->assertSame($yen200->toInt(), $dollar2->toYen()->toInt());
     }
 }
