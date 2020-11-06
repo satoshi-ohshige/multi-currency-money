b417fc7c7371579101b653cd4d1b9ff98dab91e3
takumi-ito-excite <takumi.ito@excite.jp>
2020-11-06 17:08:23 +0900

    [add] DollarオブジェクトへtoIntのテストを追加

---
+++ tests/DollarTest.php
@@ -0,0 +1,16 @@
+<?php
+declare(strict_types=1);
+
+namespace AppTest;
+
+use App\Dollar;
+use PHPUnit\Framework\TestCase;
+
+class DollarTest extends TestCase
+{
+    public function test_Dollarオブジェクトを数値に変換することのテスト()
+    {
+        $this->assertSame(100, (new Dollar(100))->toInt());
+        $this->assertSame(200, (new Dollar(200))->toInt());
+    }
+}
