fc9a2ec737c9395883b30de1afa7dc37ed3b3371
keijiro-okamura <keijiro.okamura@excite.jp>
2020-11-06 17:03:42 +0900

    [add]円をドルに変換するテストを途中まで作成

--- tests/ExchangeTest.php
+++ tests/ExchangeTest.php
@@ -19,4 +19,16 @@ class ExchangeTest extends TestCase
         $yen200 = new Yen(200);
         $this->assertSame($yen200->toInt(), $dollar2->toYen()->toInt());
     }
+
+    public function test_円をドルに変換するテスト()
+    {
+        // TODO このテストはまだ動かない
+        $yen100 = new Yen(100);
+        $dollar1 = new Dollar(1);
+        $this->assertSame($dollar1->toInt(), $yen100->toDollar()->toInt());
+
+        $yen200 = new Yen(200);
+        $dollar2 = new Dollar(2);
+        $this->assertSame($dollar2->toInt(), $yen200->toDollar()->toInt());
+    }
 }
