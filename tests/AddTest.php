5c8791d8123d489d74a184afc3910d3330940623
chie-aharen <chie.aharen@excite.jp>
2020-11-13 16:23:51 +0900

    [add]1ドル足す3ドルは4ドルになることのテストを追加

--- tests/AddTest.php
+++ tests/AddTest.php
@@ -5,6 +5,7 @@ namespace AppTest;
 
 use App\Calculator;
 use App\Yen;
+use App\Dollar;
 use PHPUnit\Framework\TestCase;
 
 class AddTest extends TestCase
@@ -26,4 +27,14 @@ class AddTest extends TestCase
         $calculator = new Calculator();
         $this->assertSame(400, $calculator->add($yen100, $yen300)->toInt());
     }
+
+    public function test_1ドル足す3ドルは4ドルになることのテスト()
+    {
+        $dollar1 = new Dollar(1);
+        $dollar3 = new Dollar(3);
+
+        $calculator = new Calculator();
+        $this->assertSame(4, $calculator->add($dollar1, $dollar3)->toInt());
+    }
+
 }
