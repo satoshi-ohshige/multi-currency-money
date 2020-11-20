e054cf32d6a229569ccfb69d8d342e017c1fb88d
motoki-ito <motoki.ito@excite.jp>
2020-11-20 17:00:57 +0900

    [add] 変換レートクラスを追加

--- src/CurrencyRate.php
+++ src/CurrencyRate.php
@@ -5,8 +5,14 @@ namespace App;
 
 class CurrencyRate
 {
-    public function __construct()
-    {
-
-    }
+    static $rate = [
+        Yen::class => [
+            Dollar::class => 0.01,
+            Yen::class => 1,
+        ],
+        Dollar::class => [
+            Yen::class => 100,
+            Dollar::class => 1,
+        ],
+    ];
 }
