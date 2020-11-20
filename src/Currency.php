2becfaf302c25acd5f0c15680341909479baba0b
yukiarai-excite <yuki.arai@excite.jp>
2020-11-20 17:39:13 +0900

    [modify] 同じ通貨は変換しない

--- src/Currency.php
+++ src/Currency.php
@@ -17,11 +17,17 @@ abstract class Currency
 
     public function toDollar(): Dollar
     {
+        if ($this instanceof Dollar) {
+            return $this;
+        }
         return new Dollar((int)($this->amount * CurrencyRate::$rate[$this::class][Dollar::class]));
     }
 
     public function toYen(): Yen
     {
+        if ($this instanceof Yen) {
+            return $this;
+        }
         return new Yen((int)($this->amount * CurrencyRate::$rate[$this::class][Yen::class]));
     }
 }
--- src/CurrencyRate.php
+++ src/CurrencyRate.php
@@ -8,11 +8,9 @@ class CurrencyRate
     static $rate = [
         Yen::class => [
             Dollar::class => 0.01,
-            Yen::class => 1,
         ],
         Dollar::class => [
             Yen::class => 100,
-            Dollar::class => 1,
         ],
     ];
 }
