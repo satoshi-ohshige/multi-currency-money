8acbd7deb82719ed2263c959b00134a6fc935c7e
Satoshi Ohshige <satoshi.ohshige@excite.jp>
2020-11-27 17:08:51 +0900

    [modify] YenとDollarのaddに関するユニットテストはそれぞれのクラスのユニットテストに移動

--- tests/Unit/DollarTest.php
+++ tests/Unit/DollarTest.php
@@ -14,4 +14,14 @@ class DollarTest extends TestCase
         $this->assertSame(100, (new Dollar(100))->toInt());
         $this->assertSame(200, (new Dollar(200))->toInt());
     }
+
+    public function test_1ドル足す3ドルは4ドルになることのテスト()
+    {
+        $dollar1 = new Dollar(1);
+        $dollar3 = new Dollar(3);
+
+        $add_result = $dollar1->add($dollar3);
+        $this->assertSame(4, $add_result->toInt());
+        $this->assertInstanceOf(Dollar::class, $add_result);
+    }
 }
--- tests/Unit/YenTest.php
+++ tests/Unit/YenTest.php
@@ -14,4 +14,25 @@ class YenTest extends TestCase
         $this->assertSame(100, (new Yen(100))->toInt());
         $this->assertSame(200, (new Yen(200))->toInt());
     }
+
+    public function test_100円足す200円は300円になることのテスト()
+    {
+        $yen100 = new Yen(100);
+        $yen200 = new Yen(200);
+
+        $add_result = $yen100->add($yen200);
+        $this->assertSame(300, $add_result->toInt());
+        $this->assertInstanceOf(Yen::class, $add_result);
+    }
+
+    public function test_100円足す300円は400円になることのテスト()
+    {
+        $yen100 = new Yen(100);
+        $yen300 = new Yen(300);
+
+        $add_result = $yen100->add($yen300);
+        $this->assertSame(400, $add_result->toInt());
+        $this->assertInstanceOf(Yen::class, $add_result);
+
+    }
 }
