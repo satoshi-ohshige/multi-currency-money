3fa6faa03aa8eebf2536d50efd3f66013992146a
Satoshi Ohshige <satoshi.ohshige@excite.jp>
2020-11-27 17:06:55 +0900

    [modify] ユニットテストはそれ専用のディレクトリに移動

--- tests/Unit/DollarTest.php
+++ tests/Unit/DollarTest.php
@@ -1,7 +1,7 @@
 <?php
 declare(strict_types=1);
 
-namespace AppTest;
+namespace AppTest\Unit;
 
 use App\Dollar;
 use App\Yen;
--- tests/Unit/YenTest.php
+++ tests/Unit/YenTest.php
@@ -1,7 +1,7 @@
 <?php
 declare(strict_types=1);
 
-namespace AppTest;
+namespace AppTest\Unit;
 
 use App\Yen;
 use App\Dollar;
